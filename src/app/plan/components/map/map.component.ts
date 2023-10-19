56335d780affb034bcdcb5bf8ff3c5f72c1e059d
Axel Herman <a.herman@afelio.be>
2023-10-19 16:49:25 +0200

    css + tooltip placement

--- src/app/plan/components/map/map.component.ts
+++ src/app/plan/components/map/map.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Inject, ViewChild } from '@angular/core';
 import { Subject } from 'rxjs';
 import { BridgesNameEnum } from '../../../enums/bridge-name.enum';
 import { BuildingNameEnum } from '../../../enums/building-name.enum';
 import { Pointer } from '../../models/pointer.model';
 import { PlanService } from '../../services/plan/plan.service';
 import { PointerName } from '../../types/pointer-name.type';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-map',
@@ -22,7 +23,7 @@ export class MapComponent {
     public pointerFocusName: PointerName | null = null;
     public selectedItem$ = new Subject<Pointer | null>();
 
-    constructor(private planService: PlanService) { }
+    constructor(private planService: PlanService, @Inject(DOCUMENT) private document: Document) { }
 
     public handlePointerClick(e: Event, pointerClicked: PointerName) {
         this.planService.getPointer(pointerClicked).subscribe( pointer => {
@@ -32,8 +33,18 @@ export class MapComponent {
                 // settimeou because of ngif need to render the elementRef before act on it
                 setTimeout(() => {
                     const tooltip: HTMLDivElement = this.pointerTooltip.nativeElement;
-                    tooltip.style.top = `${(e as PointerEvent).y}px`;
-                    tooltip.style.left = `${(e as PointerEvent).x}px`;
+                    let x = (e as PointerEvent).x;
+                    let y = (e as PointerEvent).y;
+                    const windowHeight = this.document.documentElement.clientHeight;
+                    const windowWidth = this.document.documentElement.clientWidth;
+                    if (x > windowWidth/2) {
+                        x = x - tooltip.clientWidth;
+                    }
+                    if (y > windowHeight/2) {
+                        y = y - tooltip.clientHeight;
+                    }
+                    tooltip.style.top = `${y}px`;
+                    tooltip.style.left = `${x}px`;
                 }, 0);
             }
         });
@@ -43,5 +54,4 @@ export class MapComponent {
         this.pointerFocusName = null;
         this.selectedItem$.next(null);
     }
-
 }
